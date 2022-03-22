9adf328ab5fb1b5aec833771e03bae7bace52e8a
Timothy Wu <twu23@buffalo.edu>
2022-03-22 03:09:54 -0400

    Basic functionality done, need to fix bugs

--- myScript.js
+++ myScript.js
@@ -1,6 +1,12 @@
 const numButtons = document.querySelectorAll('.number');
+const opButtons = document.querySelectorAll('.operator');
+const equalButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
 const outerDisplay = document.getElementById('outerDisplay');
 const innerDisplay = document.getElementById('innerDisplay');
+let firstValue = '';
+let currValue = '';
+let operateWith = '';
 
 function add(num1, num2){
     return num1 + num2;
@@ -32,3 +38,47 @@ function operate(operator, firstNum, secondNum){
         return divide(firstNum, secondNum);
     }
 }
+
+function computeCurrNums(){
+    let result = operate(operateWith, parseInt(firstValue), parseInt(currValue));
+    firstValue = result;
+    currValue = '';
+    innerDisplay.textContent = result;
+}
+
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(operateWith.length !== 0){
+            outerDisplay.textContent += button.textContent;
+            let temp = button.textContent;
+            currValue += temp;
+        }
+        else{
+            outerDisplay.textContent += button.textContent;
+            firstValue = outerDisplay.textContent;
+        }
+    });
+});
+
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(firstValue && currValue){
+            computeCurrNums();
+        }
+        operateWith = button.textContent;
+        outerDisplay.textContent += operateWith;
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    computeCurrNums();
+});
+
+clearButton.addEventListener('click', () => {
+    firstValue = '';
+    currValue = '';
+    operateWith = '';
+    innerDisplay.textContent = '';
+    outerDisplay.textContent = '';
+});
+
